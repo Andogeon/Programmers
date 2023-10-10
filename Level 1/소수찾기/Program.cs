695b13b09803e39b73949061e26cdf4faa834e7e
[githun <[github>
2023-10-11 01:37:20 +0900

    프로그래머 Level 1

---
+++ Level 1/소수찾기/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = solution(10);
+
+            Console.Write(result);
+        }
+
+        // 문제 11
+        // 소수 찾기
+        public static int solution(int n)
+        {
+            int[] _ArrayValue = new int[n];
+
+            for (int i = 1; i <= n; ++i)
+                _ArrayValue[i - 1] = i;
+
+            int Number = 2;
+
+            int Count = 0;
+
+            for (int i = 0; i < _ArrayValue.Length; ++i)
+            {
+                for (int j = Number + i; j <= _ArrayValue.Length; j += (Number + i))
+                {
+                    if (j == Number + i)
+                        continue;
+
+                    _ArrayValue[j - 1] = 0;
+                }
+            }
+
+            for(int i = 0; i < _ArrayValue.Length; ++i)
+            {
+                if (_ArrayValue[i] <= 1)
+                    continue;
+
+                Count += 1;
+            }
+
+            return Count;
+        }
+    }
+}
